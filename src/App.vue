8275ff9abc36897221aa64a5c76400ad02232506
cexbrayat <cedric@ninja-squad.com>
2020-07-23 18:45:52 +0200

    chore: init

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <HelloWorld :counter="counter"/>
+  <button @click="counter++">Click</button>
+</template>
+
+<script lang="ts">
+import { defineComponent, ref } from 'vue'
+import HelloWorld from './components/HelloWorld.vue'
+
+export default defineComponent({
+  name: 'App',
+  components: {
+    HelloWorld
+  },
+  setup() {
+    const counter = ref(0);
+    return { counter };
+  }
+})
+</script>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,20 @@
+<template>
+  <h1>{{ counter }}</h1>
+</template>
+
+<script lang="ts">
+import { defineComponent, toRefs, watchEffect } from 'vue'
+
+export default defineComponent({
+  name: 'HelloWorld',
+  props: {
+    counter: Number
+  },
+  setup(props) {
+    const { counter } = toRefs(props);
+    watchEffect(() => {
+      console.log('The new counter value is: ' + counter.value)
+    })
+  }
+})
+</script>
