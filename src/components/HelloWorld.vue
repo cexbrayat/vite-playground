db2d3ad2ef10e9281811a5e355068b072a77da98
cexbrayat <cedric@ninja-squad.com>
2020-07-30 16:02:58 +0200

    repro: infinite loop with watch stop handle

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,20 +1,29 @@
 <template>
-  <h1>{{ counter }}</h1>
+  <h1>{{ price }}</h1>
+  <ul>
+    <li v-for="(change, index) in history" :key="index">{{ change }}</li>
+  </ul>
 </template>
 
 <script lang="ts">
-import { defineComponent, toRefs, watchEffect } from 'vue'
+import { defineComponent, ref, watchEffect } from 'vue'
 
 export default defineComponent({
-  name: 'HelloWorld',
-  props: {
-    counter: Number
-  },
-  setup(props) {
-    const { counter } = toRefs(props);
-    watchEffect(() => {
-      console.log('The new counter value is: ' + counter.value)
-    })
+  name: 'User',
+  setup() {
+    const price = ref(10);
+
+    const history = ref<Array<string>>([]);
+    const stopRecording = watchEffect(() => {
+      history.value.push(`Price changed to ${price.value}`);
+    });
+
+    // stop recording after 3 seconds
+    setTimeout(() => {
+      stopRecording();
+    }, 3000);
+
+    return { price, history };
   }
 })
 </script>
